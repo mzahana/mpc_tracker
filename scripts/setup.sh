e5175fb6f4b9bfe5ed31ba70716ca6c2b9e26376
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2022-11-13 12:40:14 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -58,7 +58,7 @@ for p in $PKGS; do
     if [ ! -d "$HOME/catkin_ws/src/$p" ]; then
         echo "Didn't find $p. Cloning it..."
         cd $HOME/catkin_ws/src
-        git clone https://${GIT_TOKEN}@github.com/mzahana/$p
+        git clone https://${GIT_USER}:${GIT_TOKEN}@github.com/mzahana/$p
     else
         echo "$p is found. Pulling latest code..."
         cd $HOME/catkin_ws/src/$p && git pull
